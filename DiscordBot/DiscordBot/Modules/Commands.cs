f8d37958d44f67f055a7bdc92da221dc53304a6f
cpytlik <35233876+cpytlik@users.noreply.github.com>
2019-10-25 14:25:16 -0600

    Base program
    This is the core of the bot

---
+++ DiscordBot/DiscordBot/Modules/Commands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+  
+        [Command("Hi")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Hello world!");
+        }
+        [Command("Hello")]
+        public async Task Hello()
+        {
+            var user = Context.Guild.GetUser(Context.User.Id).Nickname;
+
+            await ReplyAsync("Heyo " + user + "!");
+
+        }
+
+    }
+}
